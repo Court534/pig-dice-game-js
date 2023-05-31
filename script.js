c2924c65e642e3aa210484e4c8b6b210e1342bdf
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-31 19:33:20 +0100

    Now working on the hold dice function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 // Player/Score elements
+const player0 = document.querySelector(".player--0");
+const player1 = document.querySelector(".player--1");
 const score0 = document.querySelector("#score--0");
 const score1 = document.querySelector("#score--1");
 const current0 = document.querySelector("#current--0");
@@ -38,7 +40,19 @@ rollDiceBtn.addEventListener("click", function () {
       currentScore;
   } else {
     // Switch player
-    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0.classList.toggle("player--active");
+    player1.classList.toggle("player--active");
   }
 });
+
+holdDiceBtn.addEventListener("click", function() {
+  // Add current score to active players score 
+
+  // Check if score is >= 100, then player wins
+
+  // If not then switch to the next player
+})
