e5030831fc527408655451380df500ffa8fb5c79
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-31 19:23:38 +0100

    Logic now switches player if a 1 is rolled

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 "use strict";
 
-// Selecting elements
+// Player/Score elements
 const score0 = document.querySelector("#score--0");
 const score1 = document.querySelector("#score--1");
-const current0 = document.querySelector("#current--0")
-const current2 = document.querySelector("#current--1")
+const current0 = document.querySelector("#current--0");
+const current2 = document.querySelector("#current--1");
+
+// Dice/Game elements
 const diceImage = document.querySelector(".dice");
 const newGameBtn = document.querySelector(".btn--new");
 const holdDiceBtn = document.querySelector(".btn--hold");
@@ -15,7 +17,9 @@ score0.textContent = 0;
 score1.textContent = 0;
 diceImage.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling the dice
 rollDiceBtn.addEventListener("click", function () {
@@ -29,9 +33,12 @@ rollDiceBtn.addEventListener("click", function () {
   // Check if someone has rolled a one
   if (dice !== 1) {
     // Add dice to current score
-    currentScore += dice
-    current0.textContent = currentScore // Change later 
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    // Reset score
+    // Switch player
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
   }
 });
