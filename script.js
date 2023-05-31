9f6e790ee65976d6ca4471c8ea5a3f4f40ec8b79
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-31 16:03:35 +0100

    Roll dice button now works and changes img depending on roll

--- script.js
+++ script.js
@@ -4,22 +4,23 @@
 const score0 = document.querySelector("#score--0");
 const score1 = document.querySelector("#score--1");
 const diceImage = document.querySelector(".dice");
-const newGameBtn = document.querySelector(".btn--new")
-const holdDiceBtn = document.querySelector(".btn--hold")
-const rollDiceBtn = document.querySelector(".btn--roll")
+const newGameBtn = document.querySelector(".btn--new");
+const holdDiceBtn = document.querySelector(".btn--hold");
+const rollDiceBtn = document.querySelector(".btn--roll");
 
 // Starting conditions
 score0.textContent = 0;
 score1.textContent = 0;
-diceImage.classList.add("hidden")
+diceImage.classList.add("hidden");
 
 // Rolling the dice
-rollDiceBtn.addEventListener("click", function() {
-  // Generating a random dice roll 
-  const dice = Math.trunc(Math.random() * 6)
+rollDiceBtn.addEventListener("click", function () {
+  // Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
 
   // Display the dice
-
-
+  diceImage.classList.remove("hidden")
+  diceImage.src = `images/dice-${dice}.png`
   // Check if someone has rolled a one
-})
+
+});
