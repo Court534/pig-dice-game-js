9198ce4038597cfe91c556423fed441a806a292c
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-31 19:06:33 +0100

    Dice roll not gets added to current of player0

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // Selecting elements
 const score0 = document.querySelector("#score--0");
 const score1 = document.querySelector("#score--1");
+const current0 = document.querySelector("#current--0")
+const current2 = document.querySelector("#current--1")
 const diceImage = document.querySelector(".dice");
 const newGameBtn = document.querySelector(".btn--new");
 const holdDiceBtn = document.querySelector(".btn--hold");
@@ -13,19 +15,22 @@ score0.textContent = 0;
 score1.textContent = 0;
 diceImage.classList.add("hidden");
 
+let currentScore = 0;
+
 // Rolling the dice
 rollDiceBtn.addEventListener("click", function () {
   // Generating a random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
 
   // Display the dice
-  diceImage.classList.remove("hidden")
-  diceImage.src = `images/dice-${dice}.png`
+  diceImage.classList.remove("hidden");
+  diceImage.src = `images/dice-${dice}.png`;
 
   // Check if someone has rolled a one
   if (dice !== 1) {
     // Add dice to current score
-    document.querySelector()
+    currentScore += dice
+    current0.textContent = currentScore // Change later 
   } else {
     // Reset score
   }
